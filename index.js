6149306dd5eb0038e3d45128ac33b03dcbb155c9
Buddha Lo <lo_977@yahoo.com>
2024-04-02 22:53:22 -0700

    complete

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]/g, "");
+  }
+  static titleize(string) {
+    const smallWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+
+    return string
+      .split(" ")
+      .map((word, index) => {
+        if (index > 0 && smallWords.includes(word)) {
+          return word;
+        }
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  }
 }
